c50b12b45da921dcc660fce7633f756695dd00ff
Nic Dobbins <ndobb@uw.edu>
2021-05-25 12:21:45 -0700

    multi select

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,29 @@
-import commonjs from "rollup-plugin-commonjs";
-import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
-  entry: "index.js",
-  format: "umd",
-  globals: {
-    "d3-selection": "d3",
-    "d3-drag": "d3"
+  input: "index.js",
+  output: {
+      file: "dist/d3-lasso.js",
+      format: "umd",
+      name: "lasso",
+      globals: {
+        "d3-selection": "d3",
+        "d3-drag": "d3",
+        "robust-point-in-polygon": "classifyPoint"
+      }
   },
-  moduleName: "d3",
-  dest: 'build/d3-lasso.js',
   plugins: [
     nodeResolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-      extensions: [".js", ".jsx"],
-      skip: [ "d3-selection", "d3-drag","d3-dispatch"]
-    }),
+        jsnext: true,
+        main: true,
+        browser: true,
+        extensions: [".js", ".jsx"],
+        skip: [ "node_modules/d3-selection", "node_modules/d3-drag", "node_modules/d3-dispatch" ]
+      }),
     commonjs({
-      include: "node_modules/**",
-      exclude: [ "node_modules/d3-selection/"]
-    })
+        include: "node_modules/**",
+        exclude: [ "node_modules/d3-selection/"]
+      })
   ]
 };
